5eb5a75a893533600069fc702c6e6169bffbd936
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-07-04 15:03:54 +0700

    feat[auth]: Add auth provider, protectedroute, and login page to router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import AuthProvider, { useAuth } from './utilities/auth/AuthProvider.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import ProtectedRoute from './utilities/auth/ProtectedRoute.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: (
+      <AuthProvider>
+        <Outlet />
+      </AuthProvider>
+    ),
+    children: [
+      {
+        index: true,
+        element: (
+          <ProtectedRoute>
+            <App />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+  {
+    path: 'login',
+    element: <LoginPage />,
   },
 ]);
 
