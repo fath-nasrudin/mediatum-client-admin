5f96535a7e6907aed1a48aaaa31e34d9f0c02f8e
fath-nasrudin <fath.nasrudin.i@gmail.com>
2024-07-05 15:34:50 +0700

    feat[auth]: auto update access token

--- src/features/auth/AuthProvider.jsx
+++ src/features/auth/AuthProvider.jsx
@@ -37,11 +37,78 @@ const initialUser = () => {
   return localStorageToken ? jwtDecode(localStorageToken) : null;
 };
 
+const isTokenExpired = (token) => {
+  try {
+    if (!token) throw new Error('No token provided');
+
+    const decodedToken = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+
+    return decodedToken.exp < currentTime;
+  } catch (error) {
+    console.error('Error decoding token:', error);
+    return true;
+  }
+};
+
+const refreshToken = async () => {
+  try {
+    console.log('try to refresh token...');
+    const response = await fetch(
+      `${'http://localhost:3000'}/auth/refresh-token`,
+      {
+        credentials: 'include',
+      }
+    );
+
+    if (!response.ok) {
+      console.log('refresh token response not ok');
+
+      // return error
+      const errorResult = await response.json();
+      throw new Error(errorResult.message);
+    }
+
+    console.log('success to get a new access token');
+    const result = await response.json();
+
+    return { error: null, result };
+  } catch (error) {
+    logoutAction();
+    console.error('Error fetching data:', error);
+    return { error, result: null };
+  }
+};
+
 function AuthProvider({ children }) {
   const [user, setUser] = useState(initialUser);
   const [token, setToken] = useState(
     localStorage.getItem('access_token') || ''
   );
+
+  // check token expiration and refresh if needed
+  useEffect(() => {
+    if (isTokenExpired) {
+      console.log('token expired');
+      const fetchRefreshToken = async () => {
+        const { error, result } = await refreshToken();
+
+        if (error) {
+          logoutAction();
+        }
+
+        if (result) {
+          // if login success, set states
+          const accessToken = result.access_token;
+          setUser(jwtDecode(accessToken));
+          setToken(accessToken);
+          localStorage.setItem('access_token', accessToken);
+          return { error: null };
+        }
+      };
+      fetchRefreshToken();
+    }
+  }, []);
 
   async function loginAction(data) {
     const { error, result } = await login(data);
